405530fd450d16139d873928d19c1da9de723137
isailara <isailara20002@gmail.com>
2022-03-05 20:30:06 -0600

    Herencia y Polimorfismo

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.lang.reflect.Modifier;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -141,12 +142,22 @@ public class Main {
             System.out.println(figure.getName()+", Area: "+figure.getArea()+", Perimetro: "+figure.getPerimeter());
         }
 
-        /*
         if(Modifier.isAbstract(Figure.class.getModifiers())){
             System.out.println("Figure es abstracta");
-        }*/
+        }
 
         System.out.println("Se generaron: " + Figure.getFiguresCreated() + " figuras");
+
+        /*Salida del programa:
+        Polígono, Area: 63.0, Perímetro: 31.5
+        Polígono, Area: 70.200005, Perímetro: 31.2
+        Rectángulo, Area: 20.0, Perímetro: 18.0
+        Rectángulo, Area: 43.2, Perímetro: 28.2
+        Círculo, Area: 88.247345, Perímetro: 33.300884
+        Círculo, Area: 352.98938, Perímetro: 66.60177
+        Figure es abstracta
+        Se generaron: 6 figuras
+        */
 
         /*circlesTest();
         rectanglesTest();
